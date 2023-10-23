ca1a6672ae50e99e9aa525edfa01d3c2d730bda2
MinkyeongSung <wildlegion59@gmail.com>
2023-10-23 17:01:50 +0900

    회원가입 완성

--- src/main/java/com/example/team_project/User/UserRestController.java
+++ src/main/java/com/example/team_project/User/UserRestController.java
@@ -2,9 +2,17 @@ package com.example.team_project.User;
 
 
 import lombok.RequiredArgsConstructor;
+
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.team_project.User.UserResponse.JoinResponseDTO;
+import com.example.team_project._core.utils.ApiUtils;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,8 +22,10 @@ public class UserRestController {
 
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(){
-        return null;
+    public ResponseEntity<?> join(@RequestBody @Valid UserRequest.JoinDTO requestDTO, Errors errors) {
+        userService.join(requestDTO);
+        JoinResponseDTO response = new JoinResponseDTO(requestDTO);
+        return ResponseEntity.ok().body(ApiUtils.success("회원가입 성공했어요. 굿", response));
     }
 
     @PostMapping("/login")
