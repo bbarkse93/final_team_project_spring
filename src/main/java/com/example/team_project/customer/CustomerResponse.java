3919af33e5fe1e3c76c169185b42e317e71c8854
bbarkse93 <bbarkse93@gmail.com>
2023-11-07 11:39:48 +0900

    customer init

---
+++ src/main/java/com/example/team_project/customer/CustomerResponse.java
@@ -0,0 +1,35 @@
+package com.example.team_project.customer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class CustomerResponse {
+
+    // 문의 전체 조회
+    @Getter
+    @Setter
+    public static class CustomerListRespDTO{
+
+    }
+
+    // 문의 상세 조회
+    @Getter
+    @Setter
+    public static class CustomerDetailRespDTO{
+
+    }
+
+    // 문의 등록
+    @Getter
+    @Setter
+    public static class CustomerWriteRespDTO{
+
+    }
+
+    // 문의 삭제
+    @Getter
+    @Setter
+    public static class CustomerDeleteRespDTO{
+
+    }
+}
