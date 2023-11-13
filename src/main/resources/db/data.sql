0945b34dce5053048cf6412b4c1ab4ab340ac3b0
knh1766 <knh1766@naver.com>
2023-11-13 14:32:22 +0900

    더미추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -8,6 +8,7 @@ INSERT INTO board_category_tb (`id`, `category`) VALUES (5, '취미생활');
 -- User 정보
 INSERT INTO user_tb (`id`,`username`, `password`, `email`, `nickname`, `user_pic_url`, `location`, `distinguish`, `user_created_at`) VALUES (1, 'ssar', '1q2w3e4r!', 'ssar@nate.com', '쌀', '.\images\default-profile.png', '부전동', true, NOW());
 INSERT INTO user_tb (`id`,`username`, `password`, `email`, `nickname`, `user_pic_url`, `location`, `distinguish`, `user_created_at`) VALUES (2, 'cos', '1q2w3e4r!', 'cos@nate.com', '코스',  '.\images\default-profile.png', '전포동', true, NOW());
+INSERT INTO user_tb (`id`,`username`, `password`, `email`, `nickname`, `user_pic_url`, `location`, `distinguish`, `user_created_at`) VALUES (3, 'love', '1q2w3e4r!', 'love@nate.com', '러브',  '.\images\default-profile.png', '연산동', true, NOW());
 
 -- Product 정보
 INSERT INTO product_tb (`id`,`product_name`, `product_description`, `product_price`, `product_created_at`, `user_id`, `sale_status`) VALUES (1, '다이슨 에어랩', '혹시 머리를 길러볼까해서 미리 구매했는데 팔려구요', 350000, NOW(), 1, 'true');
@@ -77,8 +78,8 @@ INSERT INTO board_tb (`id`, `board_title`, `board_content`, `board_created_at`, 
 INSERT INTO board_tb (`id`, `board_title`, `board_content`, `board_created_at`, `user_id`, `board_category_id`) VALUES (12, '배드민턴치실분', '모집합니다', NOW(), 2, 5);
 INSERT INTO board_tb (`id`, `board_title`, `board_content`, `board_created_at`, `user_id`, `board_category_id`) VALUES (13, '같이산책해요', '저녁에 같이 산책하신분', NOW(), 3, 4);
 INSERT INTO board_tb (`id`, `board_title`, `board_content`, `board_created_at`, `user_id`, `board_category_id`) VALUES (14, '파스타잘하죠?', '제가만들었어요 자랑글입니다.', NOW(), 2, 4);
-INSERT INTO board_tb (`id`, `board_title`, `board_content`, `board_created_at`, `user_id`, `board_category_id`) VALUES (14, '축구하자', '같이축구하실분', NOW(), 2, 5);
-INSERT INTO board_tb (`id`, `board_title`, `board_content`, `board_created_at`, `user_id`, `board_category_id`) VALUES (15, '지금공사하나요?', '지금어디공사하고있나요?', NOW(), 3, 4);
+INSERT INTO board_tb (`id`, `board_title`, `board_content`, `board_created_at`, `user_id`, `board_category_id`) VALUES (15, '축구하자', '같이축구하실분', NOW(), 2, 5);
+INSERT INTO board_tb (`id`, `board_title`, `board_content`, `board_created_at`, `user_id`, `board_category_id`) VALUES (16, '지금공사하나요?', '지금어디공사하고있나요?', NOW(), 3, 4);
 
 
 -- Board 사진
